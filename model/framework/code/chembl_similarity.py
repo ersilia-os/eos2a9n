725d579b7733ee506cc2bd61a48ae7568cd609ff
Samuel Maina <samuelmayna@gmail.com>
2023-08-01 22:17:02 +0000

    Refactored model to expose one function

--- model/framework/code/chembl_similarity.py
+++ model/framework/code/chembl_similarity.py
@@ -3,31 +3,23 @@ import requests
 import json
 import sys
 
-# Input Parameters
-input_file = open(sys.argv[1], 'r')
-Lines = input_file.readlines()[1:]
-
-data = []
-for input_smiles in Lines:
-    input_smiles = input_smiles.strip() 
-    url = 'http://130.92.106.217:8080/chemblECfp4.v1.NoJava/search.jsp?maxCount=100&type=numOfMols&maxDist=50&group2=None&sdfMol=&inputMol=&mask=0&smi=' + input_smiles + '&searchMode=searchByNum&limit=100'
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, features = 'html.parser')
-    results = soup.find_all('textarea')
-    
-    T = str(results[0]).splitlines()
-    T = [i for i in T if not ('CHEMBL' not in i)]
-    smiles_list = []
-    similarity_indices = []
-    for i in T:
-        x = i.split(" CHEMBL")[0]
-        x2 = i.split(" ")[2]
-        smiles_list.append(x) 
-        similarity_indices.append(x2)
-        
-    data+= [[smiles_list, similarity_indices]]
+def read_100_nearest(smiles):
+    data = []
+    for input_smiles in smiles:
+        input_smiles = input_smiles.strip() 
+        url = 'https://multi-fpb.gdb.tools/chemblsFP.v1.NoJava/search.jsp?maxCount=100&type=numOfMols&maxDist=50&group2=None&sdfMol=&inputMol=&mask=0&smi=' + input_smiles + '&searchMode=searchByNum&limit=100'
+        r = requests.get(url)
+        print(r)
+        soup = BeautifulSoup(r.text, features = 'html.parser')
+        results = soup.find_all('textarea')
+        print(results)
+        T = str(results[0]).splitlines() if results else []
+        T = [i for i in T if not ('CHEMBL' not in i)]
+        smiles_list = []
+        for i in T:
+            x = i.split(" CHEMBL")[0]
+            smiles_list.append(x)
+        data+= [smiles_list]
+    return data
 
 
-
-with open(sys.argv[2], 'w') as f:
-    json.dump(data, f)
