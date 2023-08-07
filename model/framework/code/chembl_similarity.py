5471fe463c568d08cffa164309831a2ecfa6688f
Samuel Maina <samuelmayna@gmail.com>
2023-08-07 15:12:04 +0300

    Removed prints

--- model/framework/code/chembl_similarity.py
+++ model/framework/code/chembl_similarity.py
@@ -9,10 +9,8 @@ def read_100_nearest(smiles):
         input_smiles = input_smiles.strip() 
         url = 'https://multi-fpb.gdb.tools/chemblsFP.v1.NoJava/search.jsp?maxCount=100&type=numOfMols&maxDist=50&group2=None&sdfMol=&inputMol=&mask=0&smi=' + input_smiles + '&searchMode=searchByNum&limit=100'
         r = requests.get(url)
-        print(r)
         soup = BeautifulSoup(r.text, features = 'html.parser')
         results = soup.find_all('textarea')
-        print(results)
         T = str(results[0]).splitlines() if results else []
         T = [i for i in T if not ('CHEMBL' not in i)]
         smiles_list = []
