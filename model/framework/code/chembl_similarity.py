1f789fbd90b59f49257f7dfa693497a3cc48db2e
GemmaTuron <gemma@ersilia.io>
2023-08-21 19:41:39 +0200

    updating readme [skip ci]

--- model/framework/code/chembl_similarity.py
+++ model/framework/code/chembl_similarity.py
@@ -1,5 +1,6 @@
 import subprocess
 import sys
+import time
 
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", bs4])
@@ -16,6 +17,7 @@ from time import sleep
 def read_100_nearest(smiles):
     data = []
     for input_smiles in smiles:
+        print(input_smiles)
         input_smiles = input_smiles.strip() 
         url_encoded_smiles = urllib.parse.quote(input_smiles)
         url = 'https://multi-fpb.gdb.tools/chemblsFP.v1.NoJava/search.jsp?maxCount=100&type=numOfMols&maxDist=50&group2=None&sdfMol=&inputMol=&mask=0&smi=' + url_encoded_smiles + '&searchMode=searchByNum&limit=100'
@@ -29,6 +31,7 @@ def read_100_nearest(smiles):
             x = i.split(" CHEMBL")[0]
             smiles_list.append(x)
         data+= [smiles_list]
+        time.sleep(15)
     return data
 
 
